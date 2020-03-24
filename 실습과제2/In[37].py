2977446fb94bd39a0cb1531111ff3c28ae38c40b
kimhyeyun <cqqwer@gmail.com>
2020-03-24 22:12:43 +0900

    matplot library 사용하여 plot(box/line/bar/histogram)으로 출력

---
+++ 실습과제2/In[37].py
@@ -0,0 +1,11 @@
+import inline
+import matplotlib.pyplot as plt
+from pandas import Series
+
+s3 = Series([1.2,2.5,-2.2,3.1,-0.8,-3.2,1.4],
+            index = ['Jan 1','Jan 2','Jan 3','Jan 4','Jan 5','Jan 6','Jan 7'])
+s3.plot(kind ='line',title ='Line plot')
+plt.show()
+
+
+
