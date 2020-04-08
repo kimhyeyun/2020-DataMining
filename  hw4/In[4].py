dde9545e72b907a60c3ad396942abb140dcbabc3
kimhyeyun <cqqwer@gmail.com>
2020-04-08 23:40:22 +0900

    data 가져와서 NaN처리와 박스플롯

---  hw4/In[4].py
+++  hw4/In[4].py
@@ -1,3 +1,5 @@
+import inline
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -5,9 +7,11 @@ data = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/bre
 data.columns = ['Sample code','Clump Thckness','Uniformity of Cell Size','Uniformity of Cell Shape','Marginal Adhesion',
                 'Single Epithelial Cell Size','Bare Nuclei','Bland Chromatin','Normal Nucleoli','Mitoses','Class']
 
-data=data.replace('?',np.NaN)
+data= data.drop(['Sample code'],axis=1)
+data = data.replace('?',np.NaN)
 
-print('Number of rows in original data = %d' % (data.shape[0]))
+data2 = data.drop(['Class'],axis=1)
+data2['Bare Nuclei'] = pd.to_numeric(data2['Bare Nuclei'])
+data2.boxplot(figsize=(20,3))
+plt.show()
 
-data2 = data.dropna()
-print('Number of rows after discarding missing values = %d' % (data2.shape[0]))
