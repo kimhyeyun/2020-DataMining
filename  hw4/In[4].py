4938f707331a98165c8e108ab88edc3ba49020f9
kimhyeyun <cqqwer@gmail.com>
2020-04-08 23:42:38 +0900

    data 가져와서 missing 값 버리기

---  hw4/In[4].py
+++  hw4/In[4].py
@@ -10,8 +10,7 @@ data.columns = ['Sample code','Clump Thckness','Uniformity of Cell Size','Unifor
 data= data.drop(['Sample code'],axis=1)
 data = data.replace('?',np.NaN)
 
-data2 = data.drop(['Class'],axis=1)
-data2['Bare Nuclei'] = pd.to_numeric(data2['Bare Nuclei'])
-data2.boxplot(figsize=(20,3))
-plt.show()
+print('Number of rows in original data = %d' % (data.shape[0]))
 
+data2 = data.dropna()
+print('Number of rows after discarding missing values = %d' % (data2.shape[0]))
